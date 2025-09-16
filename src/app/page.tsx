66ba346caac60fe6095b51d2d25c21f0062db887
Eros Vilela <eros.almeida.vilela@gmail.com>
2025-09-15 22:03:25 -0300

    use-client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-export default function HomePage(){
-  return(
-    <div>
-      <h1 className="text-6x1 font-bord, text-blue-500 hover:text-blue-50 "> texto no h1</h1>
-    </div>
-  )
+
+
+// page.tsx (server) -> menu.tsx (server) -> link (client)
+
+import clsx from "clsx";
+
+export default async function HomePage() {
+  console.log('HomePage');
+
+  return (
+      <h1
+        className={clsx(
+          'text-6xl',
+          'font-bold',
+          'text-blue-500',
+          'hover:text-blue-50',
+          'hover:bg-blue-500',
+          'transition',
+          'duration-1000',
+        )}
+
+      >
+        Texto no meu h1
+      </h1>
+    );
 }
