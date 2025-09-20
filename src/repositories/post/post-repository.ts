07ea8784c0a6b4d8094828aca0b791dd1ccfac5b
Eros Vilela <eros.almeida.vilela@gmail.com>
2025-09-20 20:26:28 -0300

    Repository

--- src/repositories/post/post-repository.ts
+++ src/repositories/post/post-repository.ts
@@ -2,6 +2,6 @@ import { PostModel } from "@/models/post/post-model";
 
 export interface PostRepository{
   findAll(): Promise<PostModel[]>
+  findById(id: string): Promise<PostModel>;
 }
 
-console.log(123)
