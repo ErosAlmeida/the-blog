c96f7c26eec63d6d1618e1f49ea84ffae5cd6750
Eros Vilela <eros.almeida.vilela@gmail.com>
2025-09-28 17:39:09 -0300

    SpinLoader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,15 @@
 
 // page.tsx (server) -> menu.tsx (server) -> link (client)
 
-import clsx from "clsx";
+import { SpinLoader } from "@/components/SpinLoader";
+
 
 export default async function HomePage() {
-  console.log('HomePage');
+
 
   return (
-      <h1
-        className={clsx(
-          'text-6xl',
-          'font-bold',
-          'text-blue-500',
-          'hover:text-blue-50',
-          'hover:bg-blue-500',
-          'transition',
-          'duration-1000',
-        )}
-
-      >
-        Texto no meu h1
-      </h1>
+    <div>
+      <SpinLoader/>
+    </div>
     );
 }
