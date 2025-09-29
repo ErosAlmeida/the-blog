44115b3aa84a4865042aa6b699a899242f8db22e
Eros Vilela <eros.almeida.vilela@gmail.com>
2025-09-29 19:36:34 -0300

    Container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from 'react';
 import { PostsList } from '@/components/PostsList';
+import { Container } from "@/components/Container";
 
 
 export default async function HomePage() {
@@ -12,6 +13,9 @@ export default async function HomePage() {
 
   return (
     <div className='text-slate-900 bg-slate-100 min-h-screen '>
+
+      <Container>
+
       <header>
         <h1 className='text-6xl font-bold text-center py-8'>AQUI É A HEADER</h1>
       </header>
@@ -23,6 +27,8 @@ export default async function HomePage() {
       <footer>
         <p className='text-6xl font-bold text-center py-8'>Footer</p>
       </footer>
+
+        </Container>
 
     </div>
     );
