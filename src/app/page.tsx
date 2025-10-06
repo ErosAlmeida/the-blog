e78b5794af6b172d3af4d9694f7da3509632df33
Eros Vilela <eros.almeida.vilela@gmail.com>
2025-10-06 12:23:06 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,20 +15,37 @@ export default async function HomePage() {
         <Header />
 
         <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-          <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+          <Link
+            className="w-full h-full object-cover object-center overflow-hidden rounded-xl"
+            href="#"
+          >
             <Image
               className="group-hover:scale-105 transition"
               src="/images/img1.png"
               width={1200}
               height={720}
               alt="Título do post"
+              priority
             />
           </Link>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
-            dolorem est dolor porro, doloribus neque, quidem mollitia
-            doloremque, ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam
-            harum blanditiis mollitia?
+          <div className="flex flex-col gap-4 sm:justify-center">
+            <time
+              className="text-slate-600 block text-sm/tight"
+              dateTime="2025-04-20"
+            >
+              20/04/2025 10:00
+            </time>
+
+            <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
+              <Link href="#">Lorem ipsum dolor sit amet consectetur</Link>
+            </h1>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
+              dolorem est dolor porro, doloribus neque, quidem mollitia
+              doloremque, ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam
+              harum blanditiis mollitia?
+            </p>
           </div>
         </section>
 
