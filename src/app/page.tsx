f57ac26f8596f7a4784d38514ad37c6b7ca9f8dd
Eros Vilela <eros.almeida.vilela@gmail.com>
2025-10-07 19:58:45 -0300

    PostHeading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
 import Link from "next/link";
 import Image from "next/image";
+import { PostHeading } from "@/components/PostHeading";
 
 export default async function HomePage() {
   return (
@@ -28,6 +29,7 @@ export default async function HomePage() {
               priority
             />
           </Link>
+
           <div className="flex flex-col gap-4 sm:justify-center">
             <time
               className="text-slate-600 block text-sm/tight"
@@ -35,18 +37,17 @@ export default async function HomePage() {
             >
               20/04/2025 10:00
             </time>
-
-            <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
-              <Link href="#">Lorem ipsum dolor sit amet consectetur</Link>
-            </h1>
-
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
-              dolorem est dolor porro, doloribus neque, quidem mollitia
-              doloremque, ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam
-              harum blanditiis mollitia?
-            </p>
           </div>
+
+          <PostHeading as="h1" url="#">
+            Rerum, vel ex? Impedit ullam harum blanditiis mollitia?
+          </PostHeading>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
+            dolorem est dolor porro, doloribus neque, quidem mollitia
+            doloremque, ad perspiciatis fugiat.{" "}
+          </p>
         </section>
 
         <Suspense fallback={<SpinLoader />}>
