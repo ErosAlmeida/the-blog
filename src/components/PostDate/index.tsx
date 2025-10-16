52994d54d5dc5f51782b98c211eb0ed77a7d23bf
Eros Vilela <eros.almeida.vilela@gmail.com>
2025-10-15 21:16:07 -0300

    SinglePost

---
+++ src/components/PostDate/index.tsx
@@ -0,0 +1,17 @@
+import { formatDatetime, formatDisctanceToNow } from "@/utils/format-datetime";
+
+type PostDateProps = {
+  dateTime: string;
+};
+
+export function PostDate({ dateTime }: PostDateProps) {
+  return (
+    <time
+      className="text-slate-600 text-sm/tight"
+      dateTime={dateTime}
+      title={formatDisctanceToNow(dateTime)}
+    >
+      {formatDatetime(dateTime)}
+    </time>
+  );
+}
