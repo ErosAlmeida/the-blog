c77a8b893fe103b1c5357663a135f405a228abb9
Eros Vilela <eros.almeida.vilela@gmail.com>
2025-10-18 19:34:38 -0300

    Server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,20 @@ import { Suspense } from "react";
 import { PostsList } from "@/components/PostsList";
 
 import { PostFeatured } from "@/components/PostFeatured";
+import { ClientComponent } from "@/components/ClientComponet";
+import { ServerComponent } from "@/components/ServerComponent";
 
 export default async function HomePage() {
   return (
     <>
-      <div className="text-slate-900 bg-slate-100 min-h-screen ">
-        <Suspense fallback={<SpinLoader className="min-h-20 mb-16" />}>
-          <PostFeatured />
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
 
-          <PostsList />
-        </Suspense>
-      </div>
+      <Suspense fallback={<SpinLoader className="min-h-20 mb-16" />}>
+        <PostFeatured />
+        <PostsList />
+      </Suspense>
     </>
   );
 }
