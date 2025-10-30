7d410065114142d4604969f1ed63c2d185f5d571
Eros Vilela <eros.almeida.vilela@gmail.com>
2025-10-30 18:42:44 -0300

    useCache

--- src/actions/revalidate-example.ts
+++ src/actions/revalidate-example.ts
@@ -1,10 +1,13 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
 
 export async function revalidateExampleAction(formData: FormData) {
   const path = formData.get("path") || "";
   console.log("Estou em uma server action", path);
 
-  revalidatePath(`${path}`);
+  // revalidatePath(`${path}`);
+
+  revalidateTag("posts"); // home
+  revalidateTag("post-rotina-matinal-de-pessoas-altamente-eficazes"); // single
 }
