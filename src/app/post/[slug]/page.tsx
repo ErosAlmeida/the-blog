9e6c8154f2a627e14a3ce7097498cfd6051df57d
Eros Vilela <eros.almeida.vilela@gmail.com>
2025-10-31 00:11:42 -0300

    new queries

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { SinglePost } from "@/components/SinglePost";
 import { SpinLoader } from "@/components/SpinLoader";
-import { findPostBySlugCached } from "@/lib/post/queries";
+import { findPuclicPostBySlugCached } from "@/lib/post/queries/public";
 import { Metadata } from "next";
 import { Suspense } from "react";
 
@@ -14,7 +14,7 @@ export async function generateMetadata({
 }: PostSlugPageProps): Promise<Metadata> {
   const { slug } = await params;
 
-  const post = await findPostBySlugCached(slug);
+  const post = await findPuclicPostBySlugCached(slug);
   return {
     title: post.title,
     description: post.excerpt,
